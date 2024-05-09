5a07f12365db838169fdb4f409f3075f5342160a
dream023se <dubeysanchika82@gmail.com>
2024-05-09 17:33:44 +0530

    center

--- countrysearch/src/App.js
+++ countrysearch/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {getCountryFlag}  from './API/API';
+import Countries from './component/Countries/Countries'
+import { useEffect } from 'react';
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const[flagData,setflagData]=useState([])
+  
+  const getData = async()=>{
+    const response= await getCountryFlag();
+    setflagData(response)
+  }
+  useEffect(()=>{
+    getData();
+  },[])
+    return (
+      <>
+      <Countries data={flagData}/>
+      </>
+     
+    );
+  }
+  
+
 
 export default App;
+//https://x-country-search-ecru.vercel.app/
---
+++ countrysearch/src/component/Countries/Countries.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Cards from '../Cards/Cards'
+import styles from './Countries.module.css';
+const Countries = ({data}) => {
+    console.log(data)
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (event) => {
+      setSearchTerm(event.target.value);
+    };
+    const filteredCountries = data.filter(country =>
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      
+    );
+  return (
+    <>
+    <div className={styles.search}>
+      <input
+        type="text"
+        placeholder="Search for countries..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      </div>
+    <div className={styles.row}>
+      {data.length>0?(filteredCountries.map((items)=><Cards data={items}/>)):("No Data Available")}
+    </div>
+    </>
+  )
+}
+
+export default Countries;
